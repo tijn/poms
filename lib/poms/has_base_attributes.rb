eb4e4eb102c0d5f2f55670484b14a8c27ba71f2c
Niels Hamaker <niels@brightin.nl>
2013-08-26 17:17:23 +0200

    updated default title

--- lib/poms/has_base_attributes.rb
+++ lib/poms/has_base_attributes.rb
@@ -6,12 +6,20 @@ module Poms
     
     module InstanceMethods
       def title
-        @titel ||= titles.first.value rescue nil
+        return @title if @title
+        main_title = select_title_by_type 'MAIN'
+        sub_title = select_title_by_type 'SUB' 
+
+        @titel = [main_title, sub_title].compact.join(": ") 
       end
       
       def description
         @description ||= descriptions.first.value rescue nil
       end 
+      private
+      def select_title_by_type(type)
+        titles.select{|t| t.type == type}.map(&:value).first
+      end
     end
     
     def self.included(receiver)
