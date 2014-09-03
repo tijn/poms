567be49ccb118b1e74b8cc42bad8db941de58c85
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2014-09-03 15:55:42 +0200

    fix description of spec
    it appears to test something else than it said it would

--- spec/lib/poms/schedule_event_spec.rb
+++ spec/lib/poms/schedule_event_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe Poms::ScheduleEvent do
   let(:poms_broadcast) { Fabricate(:poms_broadcast) }
   let(:schedule_event) {poms_broadcast.schedule_events.first}
-  
+
   it 'sets the start time' do
     schedule_event.starts_at.should eq(Time.parse '2013-05-28 18:08:55 +0200')
   end
-  
-  it 'sets the start time' do
+
+  it 'knows the end time' do
     schedule_event.ends_at.should eq(Time.parse '2013-05-28 18:26:24 +0200')
   end
 
